de3a40f34870c745ea234a51c3fe06cf67fa003f
Kamleshmadan <86570663+Kamleshmadan@users.noreply.github.com>
2022-02-11 01:13:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 var names=new Array();
 names[0]="Kamlesh";
-names[1]="prathmesh";
+names[1]="Jayesh";
 names[2]="madan";
-names[3]="sid";
-names[4]="akash";
-names[5]="shiv";
-names[6]="shardul";
-names[7]="suraj";
-names[8]="shantanu";
-names[9]="omkar";
+names[3]="Jash";
+names[4]="sid";
+names[5]="jay";
+names[6]="akash";
+names[7]="jain";
+names[8]="Shiv";
+names[9]="jivika";
 
 
 for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='s'|| names[i].charAt(0)==='S'){
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
         console.log("Goodbye "+ names[i])
 	}
 	else{
